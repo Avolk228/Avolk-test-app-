77045aba163b9853f7155fee88530c35d1808c08
AVolk <volkodavartem01@mail.ru>
2022-02-01 12:45:57 +0300

    входной экран готов

---
+++ lib/UI/main_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:test1/resources/app_colors.dart';
+
+import 'ad_screen.dart';
+
+class MainPage extends StatelessWidget {
+  final String title;
+  MainPage({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.mainColor,
+        centerTitle: true,
+        title: Text(
+          title,
+        ),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AdScreen(
+                  title: '',
+                ),
+              ),
+            );
+          },
+          child: Text(
+            'Перейти на экран с объявлением',
+            style: TextStyle(color: AppColors.mainColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
