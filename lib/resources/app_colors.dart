4099beef3d04bf1c90c0972e59a67361e2dd26fc
AVolk <volkodavartem01@mail.ru>
2022-02-02 16:56:11 +0300

    добавил новый ресурсы

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static final mainColor = Color.fromRGBO(41, 137, 89, 1);
+  static const mainColor = Color.fromRGBO(41, 137, 89, 1);
+  static const numberColor = Color.fromRGBO(207, 233, 220, 1);
+  static const numberTextColor = Color.fromRGBO(51, 70, 60, 1);
 }
