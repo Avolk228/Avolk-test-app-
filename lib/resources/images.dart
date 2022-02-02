d815cbb14e72a0c36de1545c755705af6c8e7b75
AVolk <volkodavartem01@mail.ru>
2022-02-02 12:56:51 +0300

    добавил некоторые ресурсы

---
+++ lib/resources/images.dart
@@ -0,0 +1,8 @@
+class Images {
+  static final List<String> imgList = [
+    'https://chudo-prirody.com/uploads/posts/2021-08/1628938755_1-p-russkii-kot-shlepa-foto-1.jpg',
+    'https://pbs.twimg.com/media/ExXrK8qWgAEZH5k.png:large',
+    'https://i.ytimg.com/vi/4f4GjgkXTxM/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/Bhv9D9_aAZg/maxresdefault.jpg',
+  ];
+}
