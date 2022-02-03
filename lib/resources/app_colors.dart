3da200f85539d8f73bc9365781b6cb6e5ff826a8
AVolk <volkodavartem01@mail.ru>
2022-02-03 20:02:00 +0300

    добавил новые ресурсы

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const mainColor = Color.fromRGBO(41, 137, 89, 1);
   static const numberColor = Color.fromRGBO(207, 233, 220, 1);
   static const numberTextColor = Color.fromRGBO(51, 70, 60, 1);
+  static const infoPartTitlecolor = Color.fromRGBO(151, 151, 153, 1);
 }
---
+++ lib/resources/info_part_strings.dart
@@ -0,0 +1,15 @@
+class InfoPartDataStrings {
+  static const adID = 'RF488918';
+  static const gender = 'Мужской';
+  static const startDate = 'Вт, 21.09.2021';
+  static const finalDate = 'Вт, 21.09.2021';
+  static const owner = 'Владимир';
+}
+
+class InfoPartTitleStrings {
+  static const adID = 'Номер объявления';
+  static const gender = 'Пол питомца';
+  static const startDate = 'Добавлено';
+  static const finalDate = 'Найден(а)';
+  static const owner = 'Имя хозяина';
+}
