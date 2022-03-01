1ace720d016cf6e1fe7f37501cd3ba54a4c78be7
AVolk <volkodavartem01@mail.ru>
2022-03-01 17:27:14 +0300

    добавил кнопку поделиться

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
   static const mainColor = Color.fromRGBO(41, 137, 89, 1);
   static const numberColor = Color.fromRGBO(207, 233, 220, 1);
   static const numberTextColor = Color.fromRGBO(51, 70, 60, 1);
   static const infoPartTitlecolor = Color.fromRGBO(151, 151, 153, 1);
+  static const commonTextColor = Colors.black;
 }
--- lib/widgets/share_block.dart
+++ lib/widgets/share_block.dart
@@ -1,69 +1,45 @@
 import 'package:AvolkTestApp/UI/ad_resourses/ad_settings.dart';
 import 'package:AvolkTestApp/UI/ad_resourses/ad_strings.dart';
 import 'package:AvolkTestApp/resources/app_colors.dart';
+import 'package:AvolkTestApp/resources/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:AvolkTestApp/widgets/text_box.dart';
+import 'package:flutter_share/flutter_share.dart';
 
-void openURL(String flag) {
-  switch (flag) {
-    case 'vk':
-      break;
-    case 'share':
-      break;
-    default:
-  }
+Future<void> share() async {
+  await FlutterShare.share(
+      text: AdStrings.adressString +
+          '\n' +
+          AdStrings.descriptionString +
+          '\n' +
+          AdStrings.phoneNumberString +
+          '\n' +
+          AdStrings.phoneNumber,
+      title: AdStrings.shareString,
+      chooserTitle: AdStrings.shareString);
 }
 
 class ShareBlock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextBox(
-          data: AdStrings.shareString,
-          fontSize: 16,
-          margin: AdTextParameters.shareMargin,
-          color: Colors.black,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 22),
-          child: Row(
-            children: [
-              InkWell(
-                onTap: () => openURL('vk'),
-                child: Ink(
-                  height: 60,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                          'lib\\resources\\images\\icons8_vk_64.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              InkWell(
-                onTap: () => openURL('share'),
-                child: Ink(
-                  height: 60,
-                  width: 60,
-                  child: const Icon(
-                    Icons.share,
-                    color: AppColors.mainColor,
-                  ),
-                ),
-              ),
-            ],
+    return InkWell(
+      onTap: () => share(),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 15, horizontal: 22),
+        height: 45,
+        alignment: Alignment.center,
+        child: const Text(
+          AdStrings.shareString,
+          style: TextStyle(
+            fontSize: 13,
+            color: AppColors.numberTextColor,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(
-          height: 15,
-        ),
-      ],
+        decoration: const BoxDecoration(
+            color: AppColors.numberColor,
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+      ),
     );
   }
 }
